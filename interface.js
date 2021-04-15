4e1878eea2de8a7b0774408032edd3d95d9b787c
Leghnider <zineb.leghnider@icloud.com>
2021-04-15 15:27:56 +0100

    created interface.js

---
+++ interface.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    var office = new Office();
+    var room = new Room();
+ 
+    // $('#list-rooms').on('click', function() {
+    //     office.list();
+    // })
+
+    // $('#add-room').on('click', function() {
+    //     office.add('Meeting Room 1');
+    // })
+})
